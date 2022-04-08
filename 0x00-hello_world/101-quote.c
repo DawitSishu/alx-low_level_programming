8fef3803fd98f8d24434b1003a8be8e959284bc3
Dawitsishu <dawitalemayhu55@gmail.com>
2022-04-08 05:12:09 -0700

    hi l

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *My hello, printf task
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19", stdout);
+return (0);
+}
