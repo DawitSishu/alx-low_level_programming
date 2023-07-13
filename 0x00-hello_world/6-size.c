bbf9fe3589473d4dfd85f9a78efd3d6fadba3fd0
Dawit <89394925+DawitSishu@users.noreply.github.com>
2023-07-13 03:22:59 -0400

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entery function
+ * main - Enterry fuction
  *
  * Return: always 0 (Success)
  */
 int main(void)
 {
-char mychar;
-int myInt;
-long veryLong;
-long long veryLongLong;
-float myFloat;
-printf("Size of a char: %i byte(s) \n", sizeof(mychar));
-printf("Size of an int: %i byte(s) \n", sizeof(myInt));
-printf("Size of a long int: %i byte(s) \n", sizeof(veryLong));
-printf("Size of a long long int: %i byte(s) \n", sizeof(veryLongLong));
-printf("Size of a float: %i byte(s) \n", sizeof(myFloat));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
