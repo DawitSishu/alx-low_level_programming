20763c00bcb047478e5b7bcef0252f0075d48c64
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-13 03:19:10 -0400

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ long veryLong;
 long long veryLongLong;
 float myFloat;
 printf("Size of a char: %i byte(s) \n", sizeof(mychar));
-printf("Size of an int: %i byte(s) \n", sizeof(inmyIntt));
+printf("Size of an int: %i byte(s) \n", sizeof(myInt));
 printf("Size of a long int: %i byte(s) \n", sizeof(veryLong));
 printf("Size of a long long int: %i byte(s) \n", sizeof(veryLongLong));
 printf("Size of a float: %i byte(s) \n", sizeof(myFloat));
