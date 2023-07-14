cc04f2a1b9a5d1b98c10f2b88c42f9458e784a21
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-14 13:11:54 -0400

    quick fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,16 +6,18 @@
  */
 int main(void)
 {
-int num,num2,num3;
-for (num3=0;num3<=9;num3++) {
-for (num = num3+1; num <= 9 ; num++)
+int num, num2, num3;
+for (num3 = 0; num3 <= 9; num3++)
 {
-for(num2 = num + 1;num2 <= 9; num2++)
+for (num = num3 + 1; num <= 9; num++)
+{
+for (num2 = num + 1; num2 <= 9; num2++)
 {
 putchar(num3 + '0');
 putchar(num + '0');
 putchar(num2 + '0');
-if(num3 == 7){
+if (num3 == 7)
+{
 continue;
 }
 putchar(',');
