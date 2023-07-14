672083e434543279252956d439a27c1a8c1526d1
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-14 13:17:59 -0400

    quick fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,18 @@
 int main(void)
 {
 int num;
-char s = ',';
 for (num = '0'; num <= '9' ; num++)
 {
 putchar(num);
 if (num == '9')
 {
-s = '$';
+putchar('$');
 }
-putchar(s);
+else
+{
+putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
