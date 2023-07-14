21d6d6026ff216382b61f3621b57fb76ad53c7a6
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-14 13:13:52 -0400

    quick fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-int num,num2;
-for (num = 0; num <= 9 ; num++)
+int num, num2;
+for (num = 0; num <= 9; num++)
 {
-for(num2 = num + 1;num2 <= 9; num2++)
+for (num2 = num + 1; num2 <= 9; num2++)
 {
 putchar(num + '0');
 putchar(num2 + '0');
-if(num==8){
+if (num == 8)
 continue;
-}
 putchar(',');
 putchar(' ');
 }
