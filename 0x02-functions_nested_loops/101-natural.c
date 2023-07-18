52f21d3b0e3acd9e55feff2bea43107a04fcda2b
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-18 05:56:20 -0400

    fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,10 @@
-#include "main.h"
 #include <stdio.h>
 /**
- * add - prinsts multiple of 3 and f belo 1024
+ * main - prinsts multiple of 3 and f belo 1024
  *
- * Return: nothing
+ * Return: 0 (success)
  */
-void natural_num_sum(void)
+int main(void)
 {
 int i, sum = 0;
 for (i = 1; i < 1024; i++)
@@ -16,4 +15,5 @@ sum += i;
 } 
 }
 printf("%d\n", sum);
+return (0);
 }
