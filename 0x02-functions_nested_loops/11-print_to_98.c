c236693aa8751fa8884e0b5b038dcc3c83908555
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-18 04:45:02 -0400

    pront a number to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints the numbers upto 98
+ * @n: the number in which the couning begins
+ *
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n >= 98)
+{
+printf("%d, ", n);
+n--;
+}
+}
+else
+{
+while (n <= 98)
+{
+printf("%d, ", n);
+n++;
+}
+}
+printf("\n");
+}
