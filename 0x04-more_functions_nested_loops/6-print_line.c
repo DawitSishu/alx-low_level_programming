ddb25443d7aa053323f086c47411079c8b535322
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-20 06:43:12 -0400

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - prints a line _ with length
+ * @n : length of the line
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
