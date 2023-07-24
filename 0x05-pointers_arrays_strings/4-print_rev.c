722441ce550a1a58d6194bb08f36a884e10610fe
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-24 03:20:13 -0400

    reverse and print strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a given sring to the console in reverse
+ * @s: pointer to the sting
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+while (i >= 0)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
+}
