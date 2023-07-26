a8a4267db58c0a50edd65b67cefe27de97d541f2
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-26 07:27:42 -0400

    string with delimited n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat -  appends the src string to the dest string, it'l use at most
+ * n bytes from src; and src does not need to be null-terminated
+ * if it contains n or more bytes
+ * @dest: pointer to the final string
+ * @src: pointer to the initial string
+ * @n: number of bytes
+ *
+ * Return: nothing
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, length = n;
+while (dest[length] != '\0')
+{
+length++;
+}
+for (i = 0; i < n; i++)
+{
+dest[length] = src[i];
+length++;
+}
+return (dest);
+}
