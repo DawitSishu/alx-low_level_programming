9dce4673d3587eb265976987d84b17e279aba165
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-26 09:02:09 -0400

    capitalize the firt letters

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: pointer to the sting
+ *
+ * Return: final string
+ */
+char *cap_string(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0' ; i++)
+{
+if (s[i] == ' ' ||
+s[i] == '\t' ||
+s[i] == '\n' ||
+s[i] == ',' ||
+s[i] == ';' ||
+s[i] == '.' ||
+s[i] == '!' ||
+s[i] == '?' ||
+s[i] == '"' ||
+s[i] == '(' ||
+s[i] == ')' ||
+s[i] == '{' ||
+s[i] == '}')
+{
+if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+s[i + 1] -= 32;
+}
+}
+return (s);
+}
