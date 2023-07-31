7551df91b6b14fff4261b13ee3513e6071f25fbf
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-31 06:44:53 -0400

    fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,9 +12,7 @@ char *_strstr(char *haystack, char *needle)
 int i, j;
 for (i = 0; haystack[i] != '\0'; i++)
 {
-if (haystack[i] == needle[0])
-{
-for (j = 1; needle[j] != '\0'; j++)
+for (j = 0; needle[j] != '\0'; j++)
 {
 if (haystack[i + j] != needle[j])
 {
@@ -24,7 +22,6 @@ break;
 if (needle[j] == '\0')
 {
 return (haystack + i);
-}
 }
 }
 return ('\0');
