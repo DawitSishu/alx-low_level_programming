2a4eaa739da884934e06b378301a0d92d5bd28cf
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-04 08:51:56 -0400

    length of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-* main - prints the name
+* main - prints number of arguments
 * @argv: array of command lines
 * @argc: size of argv
 * Return: 0
 **/
 int main(int argc, char **argv)
 {
-if (argc >= 0)
+if (argv == NULL || argv != NULL)
 {
-printf("%s\n", argv[0]);
+printf("%d\n", argc - 1);
 }
 return (0);
 }
