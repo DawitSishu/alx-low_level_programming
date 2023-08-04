289a18dc1d3f5b1e769f411a908586f2939a0257
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-04 08:47:40 -0400

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @argv: array of command lines
+* @argc: size of argv
+* Return: 0
+**/
+int main(int argc, char **argv)
+{
+if (argc >= 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
