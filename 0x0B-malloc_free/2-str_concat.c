b7f9d85232f09d657e8c87769f5acc515b388c31
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-07 07:54:41 -0400

    fix null conditions

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,14 @@ char *str_concat(char *s1, char *s2)
 {
 char *final_string;
 int i, len1 = 0, len2 = 0;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
 while (s1[len1] != '\0')
 {
 len1++;
