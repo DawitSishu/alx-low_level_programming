567a8268b8bc0539153279a1f739401a25a3343f
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-07 07:34:09 -0400

    copy a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - copies a string and returns a pointer to it
+ * @str: the initial array
+ *
+ * Return: NULL if malloc fails or pointer to the array
+ */
+char *_strdup(char *str)
+{
+int i, size = 0;
+char *final_string;
+while (str[size] != '\0')
+{
+size++;
+}
+final_string = (char *)malloc(size *sizeof(char));
+if (final_string == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+final_string[i] = str[i];
+}
+return (final_string);
+}
