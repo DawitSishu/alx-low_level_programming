5a91e0dabebd7ca5cc0fcec189359fa35633a8d0
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-07 08:11:04 -0400

    segmentation fault

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,6 +15,10 @@ if (width <= 0 || height <= 0)
 return (NULL);
 }
 final_array = (int **) malloc(height * sizeof(int));
+if (final_array == NULL)
+{
+return (NULL);
+}
 for (i = 0; i < height; i++)
 {
 final_array[i] = (int *) malloc(width * sizeof(int));
