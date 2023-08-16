ee2f65f64fe37184578690df6afc38ff3c6120f5
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-16 05:26:54 -0400

    simple calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+* main - calculates the numberswith operatiors
+* @argv: array of command lines
+* @argc: size of argv
+* Return: 0 (success), 98,99,100 (failure/some error)
+**/
+int main(int argc, char **argv)
+{
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[3]);
+int (*operation)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' && *argv[2] != '/' && *argv[2] != '%')
+{
+printf("Error\n");
+exit(99);
+}
+if ((*argv[2] == '%' || *argv[2] == '/') && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+operation = get_op_func(argv[2]);
+printf("%d\n", operation(num1, num2));
+return (0);
+}
