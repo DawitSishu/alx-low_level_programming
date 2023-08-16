8c66f3e04dfadfef44ef6826181ed2012b8ed3ae
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-16 05:52:17 -0400

    fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -26,5 +26,5 @@ return (ops[i].f);
 }
 i++;
 }
-return (0);
+return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,6 +12,7 @@ int main(int argc, char **argv)
 int num1 = atoi(argv[1]);
 int num2 = atoi(argv[3]);
 int (*operation)(int, int);
+
 if (argc != 4)
 {
 printf("Error\n");
@@ -22,12 +23,12 @@ if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' && *argv[2] != '/' && 
 printf("Error\n");
 exit(99);
 }
-if ((*argv[2] == '%' || *argv[2] == '/') && num2 == 0)
+operation = get_op_func(argv[2]);
+if (operation == NULL)
 {
 printf("Error\n");
-exit(100);
+exit(99);
 }
-operation = get_op_func(argv[2]);
 printf("%d\n", operation(num1, num2));
 return (0);
 }
