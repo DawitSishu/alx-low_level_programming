52d468a181d3470393c9c4673ea5066d9d5e6d1d
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-16 03:28:03 -0400

    setup

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+/**
+*   File:function_pointers.h
+*   Author:Dawit Sishu
+*   Desc: header file that has function declarations
+*/
+#ifndef function_pointers_h
+#define function_pointers_h
+int _putchar(char);
+
+#endif
