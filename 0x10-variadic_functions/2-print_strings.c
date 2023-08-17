199f50f33f4ccbd0d96cea91d0a10bab540a7d18
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-17 04:17:56 -0400

    peint a given strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * print_numbers - prints given numbers by a separator
+ * print_strings - prints given strings by a separator
  * @n: number of arguments
- * @separator: separator betwwen the numbers
+ * @separator: separator betwwen the strings
  *
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
 va_list numbers;
 va_start(numbers, n);
 for (i = 0; i < n; i++)
 {
-printf("%d", va_arg(numbers, int));
+printf("%s", va_arg(numbers, char*));
 if (separator != NULL && (i + 1 != n))
 printf("%s", separator);
 }
