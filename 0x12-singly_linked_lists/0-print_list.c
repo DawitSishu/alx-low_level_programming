f6e138ccd50c2f91bb6081e379e4081fcd622bdc
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-24 03:46:25 -0400

    print the list and reurn the number of element

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list -  that prints all the elements of a list_t list.
+ * @h: head of the node
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+const list_t *temp = h;
+int count = 0;
+while (temp != NULL)
+{
+if (temp->str != NULL)
+{
+printf("[%d] %s\n", temp->len, temp->str);
+}
+else
+{
+printf("[0] (nil)\n");
+}
+count++;
+temp = temp->next;
+}
+return (count);
+}
