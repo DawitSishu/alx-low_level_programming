52edd22f2c014409a71f27af65506c8bff30d5e4
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-28 12:38:24 -0400

    fix

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <string.h>
 /**
-* add_node -  adds a new node at the beginning of a listint_t list.
+* add_nodeint -  adds a new node at the beginning of a listint_t list.
 * @head: actual head address of the list
 * @n: the number to be added on the head
 *
