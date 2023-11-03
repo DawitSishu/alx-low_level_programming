ab0083b5cf486ee9c36432be8d6b924f92ccaa4f
DawitSishu <dawitalemayhu55@gmail.com>
2023-11-03 02:12:17 -0400

    fixing

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,8 +7,11 @@
 *
 * Return: index of key - value pair
 */
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+if (key == NULL || size == 0)
+{
+return (0);
+}
 return (hash_djb2(key) % size);
 }
