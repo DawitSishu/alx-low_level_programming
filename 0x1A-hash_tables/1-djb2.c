f9e220ba7c655e2562bc97c3aafe3b79927f3f03
DawitSishu <dawitalemayhu55@gmail.com>
2023-11-03 02:08:35 -0400

    fixing

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,20 +1,20 @@
 #include "hash_tables.h"
 
 /**
-* hash_djb2 - djb2 algorith imolemnter
-* @str: string for hash table
+* hash_djb2 - generates a random nmber
+* @str: string
 *
 * Return: hash
 */
-
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hash_val;
-int chs
-hash_val = 5381;
-while ((chs = *str++))
+unsigned long int hashed;
+int c;
+
+hashed = 5381;
+while ((c = *str++))
 {
-hash_val = ((hash_val << 5) + hash_val) +chs;
+hashed = ((hashed << 5) + hashed) + c;
 }
-return (hash_val);
+return (hashed);
 }
