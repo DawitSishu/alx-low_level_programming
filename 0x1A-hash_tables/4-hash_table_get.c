8e1dcce8c606e41678daf3b809c249852fdbd745
DawitSishu <dawitalemayhu55@gmail.com>
2023-11-03 02:20:24 -0400

    fixing

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,12 +11,18 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 unsigned long int idx;
-hash_node_t *temp = NULL;
-if (!ht || !key || strlen(key) == 0)
+hash_node_t *temp;
+if (ht == NULL)
+{
 return (NULL);
-idx = key_idx((const unsigned char *)key, ht->size);
+}
+if (key == NULL || *key == '\0')
+{
+return (NULL);
+}
+idx = key_index((unsigned char *)key, ht->size);
 temp = ht->array[idx];
-while (temp)
+while (temp != NULL)
 {
 if (strcmp(temp->key, key) == 0)
 return (temp->value);
